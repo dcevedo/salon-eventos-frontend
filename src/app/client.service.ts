3b7c02414a258960f3f3c35293a8f7e84c4d318e
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-09-26 21:52:15 +0000

    Add environment api url

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Client } from './clients';
+import { environment } from "../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  private baseURL = "http://localhost:8080/api/clients";
+  // private baseURL = "http://localhost:8080/api/clients";
+  private baseURL =`${environment.API_URL}/clients`;
 
   constructor(private httpClient : HttpClient) { }
 
