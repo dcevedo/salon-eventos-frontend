f82262a533a6a6d9dede4a832bc35c47f779de1d
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-10-05 19:05:00 -0500

    Create message components

---
+++ src/app/messages/message-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-update',
+  templateUrl: './message-update.component.html',
+  styleUrls: ['./message-update.component.css']
+})
+export class MessageUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
