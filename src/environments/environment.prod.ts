d0990db22adb260636afee714092a871c43a55f9
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-10-09 19:32:13 -0500

    Add client register name pattern

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://URL:8080/api',
+  API_URL: 'http://172.28.205.13:8080/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://172.28.205.13:8080/api',
 };
 
 /*
